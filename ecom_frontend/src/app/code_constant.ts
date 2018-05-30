6d26bbcbff087d4994baf40f1f709895545fcfaa
Suraj Singh <suraj.mehta@ongraph.in>
2018-05-30 15:11:16 +0530

    change ipfs IP address

--- ecom_frontend/src/app/code_constant.ts
+++ ecom_frontend/src/app/code_constant.ts
@@ -1,4 +1,4 @@
 export class CodeConstants {
   public static readonly DECIMAL: number = 1000000000000000000;
-  public static readonly URL: string = "164.132.137.143";
+  public static readonly URL: string = "13.250.35.159";
 }
